ba8fc47adc455f6f60273997201a45bace6627c3
David Francisco <dfnb@cin.ufpe.br>
2019-12-10 05:33:12 +0000

    melhorias de peformance

--- index.js
+++ index.js
@@ -2,62 +2,37 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const Docker = require('dockerode');
-const docker = new Docker({socketPath: '/var/run/docker.sock'});
-app.use(bodyParser.json());
-const axios = require('axios');
-const User = {
-	id: {
-		unique: true,
-		primaryKey: true,
-		autoIncrement: true,
-		type: Number
-	},
-	userid: {
-		type: String,
-		allowNull: false
-	             },
-	password: {
-	        type: String
-	        },
-	keyApi: {
-		type: String
-	},
-	workers: {
-		type: Number,
-		defaultValue: 0
-	},
-	managers: {
-		type: Number,
-		defaultValue: 1
-	}
-	};
 
-app.get('/', async (req, res) => res.json(await docker.listContainers()));
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
+const {Docker} = require('node-docker-api')
+const docker = new Docker({ socketPath: '/var/run/docker.sock' })
+
+app.use(bodyParser.json())
 
 app.post('/cluster', async (req,res) => {
-	const containers = await docker.listContainers()
+	try{
+	const containers = await docker.container.list()
 	const totalContainer = containers.length + 1
-/*
-	const newOne = await docker.createContainer({
-		  Image: 'docker',
-		  name: res.keyApi,
-		  AttachStdin: false,
-		  AttachStdout: true,
-		  AttachStderr: true,
-		  Tty: true,
-		  OpenStdin: false,
-		  StdinOnce: false,
-		  privileged: true,
-		  PortBindings: {"2375/tcp": [{HostPort: totalContainer + "2375"}]} 
-	})
-	await newOne.start()
-*/
-	docker.run('docker', ['bash', '-c', 'uname -a'], [process.stdout, process.stderr], {Tty:false}, function (err, data, container) {
-		  console.log(err);
-	});
-	res.status(200).json({port : totalContainer + 2375, url: res.keyApi})
+	const { stderr: stderrR,stdout: stdoutR } = await exec(`docker run -d --privileged --name ${req.body.keyApi} -p ${totalContainer}2375:2375 docker:18-dind`)
+	console.log(stderrR)
+	console.log(stdoutR)
+	const { stderr : stderrT, stdout: stdoutT } = await exec(`docker -H tcp://localhost:${totalContainer}2375 swarm init`)
+	console.log(stderrT)
+	console.log(stdoutT)
+	const { stdout: SWARM_TOKEN, stderr: fail } = await exec(`docker -H tcp://localhost:${totalContainer}2375 swarm join-token -q worker`)
+	console.log(SWARM_TOKEN)
+	console.log(fail)
+	const list = await Promise.all([...Array(req.body.worker).keys()].map(i => exec(`docker -H tcp://localhost:${totalContainer}2375 run -d --privileged --name worker-${i+1} --hostname=worker-${i+1} -p ${i+1}2375:2375 docker:18-dind`)))
+	list.map(x => {console.log(x.stderr);console.log(x.stdout)})
+	const listL = await Promise.all([...Array(req.body.worker).keys()].map(i => exec(`SWARM_MASTER_IP=$(docker exec ${req.body.keyApi} docker exec worker-${i+1} ip route show | awk '/default/ {print $3}')
+		docker exec ${req.body.keyApi} docker exec worker-${i+1} docker swarm join --token ${SWARM_TOKEN.trim()} \${SWARM_MASTER_IP}:2377`)))
+	listL.map(x => {console.log(x.stderr);console.log(x.stdout)})
+	res.status(200).json({port : totalContainer + "2375", url: res.keyApi})
+	}catch(err){
+	res.status(400).json({message: err})
+	}
 })
 
 app.listen(port)
---
+++ run.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+	  --request POST \
+	    --data '{"keyApi":"3423dfdsa", "worker" : 4}' \
+	      http://localhost:3000/cluster
